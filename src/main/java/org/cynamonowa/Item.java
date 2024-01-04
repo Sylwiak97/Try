356956e353b8800c57407e87e2dca1408812a76e
spulka <9XsVnUS9sg>
2024-01-04 09:12:06 +0100

    Item

---
+++ src/main/java/org/cynamonowa/Item.java
@@ -0,0 +1,23 @@
+package org.cynamonowa;
+
+public class Item {
+   private String name;
+   String getName(){
+       return this.name;
+   }
+   private double cost;
+    double getCost(){
+        return this.cost;
+    }
+   private String shop;
+    String getShop(){
+        return this.shop;
+    }
+   private int count;
+    int getCount(){
+        return this.count;
+    }
+
+
+
+}
