85d9cf0256b47b7d967e651887913fae10a43875
spulka <9XsVnUS9sg>
2024-01-04 09:42:24 +0100

    Item

--- src/main/java/org/cynamonowa/Item.java
+++ src/main/java/org/cynamonowa/Item.java
@@ -5,17 +5,29 @@ public class Item {
    String getName(){
        return this.name;
    }
+   void setName(String name){
+       this.name = name;
+   }
    private double cost;
     double getCost(){
         return this.cost;
+    }
+    void setCost(double cost){
+        this.cost = cost;
     }
    private String shop;
     String getShop(){
         return this.shop;
     }
+    void setShop(String shop){
+        this.shop = shop;
+    }
    private int count;
     int getCount(){
         return this.count;
+    }
+    void setCount(int count){
+        this.count = count;
     }
 
 
