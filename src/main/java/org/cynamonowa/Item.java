1c293feedf7cf205fb9f3e7d45d2fb61d9307bd4
spulka <9XsVnUS9sg>
2024-01-04 14:07:42 +0100

    konstruktory

--- src/main/java/org/cynamonowa/Item.java
+++ src/main/java/org/cynamonowa/Item.java
@@ -1,32 +1,43 @@
 package org.cynamonowa;
 
 public class Item {
-   private String name;
-   String getName(){
-       return this.name;
-   }
-   void setName(String name){
-       this.name = name;
-   }
-   private double cost;
-    double getCost(){
+    private String name;
+
+    String getName() {
+        return this.name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    private double cost;
+
+    double getCost() {
         return this.cost;
     }
-    void setCost(double cost){
+
+    void setCost(double cost) {
         this.cost = cost;
     }
-   private String shop;
-    String getShop(){
+
+    private String shop;
+
+    String getShop() {
         return this.shop;
     }
-    void setShop(String shop){
+
+    void setShop(String shop) {
         this.shop = shop;
     }
-   private int count;
-    int getCount(){
+
+    private int count;
+
+    int getCount() {
         return this.count;
     }
-    void setCount(int count){
+
+    void setCount(int count) {
         this.count = count;
     }
 
@@ -36,17 +47,26 @@ public class Item {
         return KindOfItem;
     }
 
-    public void setKindOfItem(org.cynamonowa.KindOfItem kindOfItem) {
+    public void setKindOfItem(KindOfItem kindOfItem) {
         KindOfItem = kindOfItem;
     }
+
     private Room Room;
 
     public org.cynamonowa.Room getRoom() {
         return Room;
     }
 
-    public void setRoom(org.cynamonowa.Room room) {
+    public void setRoom(Room room) {
         Room = room;
     }
-}
 
+    public Item(String name, String shop, double cost, int count) {
+        this.name = name;
+        this.shop = shop;
+        this.count = count;
+        this.cost = cost;
+    }
+    public Item(){}
+
+}
